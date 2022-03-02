a1bb46d0b4bb485acee8134ed2715202b1505495
Gnadi4 <uno6070w@gmail.com>
2022-03-02 23:51:39 +0900

    Time: 56 ms (32.00%), Space: 14.1 MB (55.24%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,12 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = [[]]
         
         def func(ind, s):
-            ans.append(s)
-            
             for i in range(ind, len(nums)):
-                func(i+1, s + [nums[i]])
+                s.append(nums[i])
+                ans.append(s.copy())
+                func(i+1, s)
+                s.pop()
             return
         
         func(0, [])
