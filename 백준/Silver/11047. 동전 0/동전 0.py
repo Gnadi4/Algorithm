b9915c03c3b49b425f27ed2c16c8b32feaac6fb5
Gnadi4 <uno6070w@gmail.com>
2022-03-30 14:08:30 +0900

    [Silver III] Title: 동전 0, Time: 160 ms, Memory: 125648 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n,k = map(int, input().rstrip().split())
+
+list_n = deque([])
+for _ in range(n):
+    list_n.appendleft(int(input().rstrip()))
+
+ans = 0
+for i in list_n:
+    ans += (k//i)
+    k%=i
+
+print(ans)
