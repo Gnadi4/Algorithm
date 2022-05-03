22020f2d00a1147227d01e979fd13b57801612ac
Gnadi4 <uno6070w@gmail.com>
2022-05-04 00:23:02 +0900

    [Gold V] Title: 토마토, Time: 1056 ms, Memory: 242416 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 import heapq
 
 input = sys.stdin.readline
@@ -13,16 +14,17 @@ for i in range(h):
         for k in range(m):
             map_h[i][j][k] = a[k]
 
-q = []
+q = deque([])
 chk_h = [[[1]*m for _ in range(n)] for _ in range(h)]
 res = 0
 for i in range(h):
     for j in range(n):
         for k in range(m):
             if chk_h[i][j][k] and map_h[i][j][k] == 1:
-                heapq.heappush(q,(0,i,j,k))
+                # heapq.heappush(q,(0,i,j,k))
+                q.append((0,i,j,k))
 while q:
-    num,a,b,c = heapq.heappop(q)
+    num,a,b,c = q.popleft()
     res = max(res, num)
     if chk_h[a][b][c]:
         chk_h[a][b][c] = 0
@@ -31,7 +33,7 @@ while q:
             if 0<=a+aa<h and 0<=b+bb<n and 0<=c+cc<m and chk_h[a+aa][b+bb][c+cc]:
                 if map_h[a+aa][b+bb][c+cc] == 0:
                     map_h[a+aa][b+bb][c+cc] = 1
-                    heapq.heappush(q,(num+1, a+aa, b+bb, c+cc))
+                    q.append((num+1, a+aa, b+bb, c+cc))
 
 ans = 1
 for i in range(h):
