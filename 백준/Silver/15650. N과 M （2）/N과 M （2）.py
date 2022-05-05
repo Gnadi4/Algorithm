6df4878536286b95536ca416855ed332420eb6bc
Gnadi4 <uno6070w@gmail.com>
2022-05-05 23:51:43 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+n,m = map(int, input().split())
+
+def func(q, cnt):
+    if cnt == m:
+        for i in q:
+            print(i, end=' ')
+        print()
+        return
+    if q:
+        for i in range(q[-1]+1, n+1):
+            func(q+[i], cnt+1)
+    else:
+        for i in range(1,n+1):
+            func(q+[i], cnt+1)
+    return
+
+func([],0)
