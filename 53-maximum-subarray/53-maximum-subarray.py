a01085bca4464e467e29a3c5bc77252b399766b3
Gnadi4 <uno6070w@gmail.com>
2022-05-17 15:44:52 +0900

    Time: 5400 ms (5.03%), Space: 42 MB (10.06%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        def func(l,r):
+            
+            if r-l <= 1:
+                return nums[l]
+            
+            res = max(func(l,(l+r)//2), func((l+r)//2,r))
+            
+            left_b = nums[(l+r)//2]
+            left_sum = 0
+            for i in range((l+r)//2, l-1, -1):
+                left_sum += nums[i]
+                left_b = max(left_b, left_sum)
+            right_b = 0
+            right_sum = 0
+            for i in range(((l+r)//2)+1, r):
+                right_sum += nums[i]
+                right_b = max(right_b, right_sum)
+                           
+            # print(res, right_b + left_b)
+            
+            return max(res, right_b + left_b) 
+        
+        return func(0, len(nums))
